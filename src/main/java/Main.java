d0cda7e3ed1c4c2190d19e21450b04a063b6f5db
hj145 <hj145@duke.edu>
2022-01-14 17:29:02 -0500

    Initial Commit - read stdin

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String s;
+        while((s = console.nextLine()).length()!=0)
+        {
+            System.out.println(s);
+        }
+
+    }
+}
